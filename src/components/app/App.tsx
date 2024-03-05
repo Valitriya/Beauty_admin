e2e23147c9b48be7c0c8d277bc1d3baff59d2448
Valentina <Vallitriya@gmail.com>
2024-03-05 10:40:09 +0300

    added template file headers

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,9 +1,11 @@
+import Header from "../header/Header";
 import "./app.scss";
-function App(){
-    return (
-        <main className="board">
-        </main>
-    )
+function App() {
+	return (
+		<main className="board">
+			<Header />
+		</main>
+	);
 }
 
 export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,5 @@
+import './header.scss';
+function Header(){
+
+}
+export default Header;
