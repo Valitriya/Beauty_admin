6c415d2ffaf16ed2f226e82cfb076bcb8023b671
Valentina <Vallitriya@gmail.com>
2024-03-06 17:08:54 +0300

    layout of the schedule page is done

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,11 +1,9 @@
 import Header from "../header/Header";
-import AppointmentItem from "../appointmentItem/AppointmentItem";
 import "./app.scss";
 function App() {
 	return (
 		<main className="board">
 			<Header />
-			<AppointmentItem/>
 		</main>
 	);
 }
---
+++ src/pages/schedule/SchedulePage.tsx
@@ -0,0 +1,18 @@
+import Calendar from "../../components/calendar/Calendar";
+import CAForm from "../../components/createAppointmentForm/CAForm";
+import AppointmentList from "../../components/appointmentList/AppointmentList";
+function SchedulePage() {
+	return (
+		<section className="schedule">
+			<div className="schedule__controls">
+				<Calendar />
+				<CAForm />
+			</div>
+			<div className="schedule__list">
+				<AppointmentList />
+			</div>
+		</section>
+	);
+}
+
+export default SchedulePage;
