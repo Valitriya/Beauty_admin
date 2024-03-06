0289407bd3380e60334c1e6e95416143548affba
Valentina <Vallitriya@gmail.com>
2024-03-06 18:04:04 +0300

    added style for the schedule page

--- src/pages/schedule/SchedulePage.tsx
+++ src/pages/schedule/SchedulePage.tsx
@@ -1,6 +1,8 @@
 import Calendar from "../../components/calendar/Calendar";
 import CAForm from "../../components/createAppointmentForm/CAForm";
 import AppointmentList from "../../components/appointmentList/AppointmentList";
+
+im
 function SchedulePage() {
 	return (
 		<section className="schedule">
