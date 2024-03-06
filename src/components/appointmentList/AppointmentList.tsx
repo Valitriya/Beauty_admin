c46a784e5080f925fddfac6e192a9b9300ff0c47
Valentina <Vallitriya@gmail.com>
2024-03-06 17:01:39 +0300

    added appointment list

---
+++ src/components/appointmentList/AppointmentList.tsx
@@ -0,0 +1,13 @@
+import AppointmentItem from "../appointmentItem/AppointmentItem";
+function AppointmentList(){
+    return(
+        <>
+            <AppointmentItem/>
+            <AppointmentItem/>
+            <AppointmentItem/>
+            <AppointmentItem/>
+        </>
+    )
+}
+
+export default AppointmentList;
