2c22beae389df8d1f6fd5357344282fd4225117e
Valentina <Vallitriya@gmail.com>
2024-03-10 18:23:27 +0300

    added modal window style

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,9 +1,11 @@
 import Header from "../header/Header";
+import CancelModal from "../modal/CancelModal";
 import "./app.scss";
 function App() {
 	return (
 		<main className="board">
 			<Header />
+			<CancelModal />
 		</main>
 	);
 }
--- src/components/modal/CancelModal.tsx
+++ src/components/modal/CancelModal.tsx
@@ -1,14 +1,18 @@
+import "./modal.scss";
+
 function CancelModal() {
 	return (
 		<div className="modal">
-			<span className="modal__title">
-				Are you sure you want to delete the appointment?
-			</span>
-			<div className="modal__btns">
-				<button className="modal__ok">Ok</button>
-				<button className="modal__close">Close</button>
+			<div className="modal__body">
+				<span className="modal__title">
+					Are you sure you want to delete the appointment?
+				</span>
+				<div className="modal__btns">
+					<button className="modal__ok">Ok</button>
+					<button className="modal__close">Close</button>
+				</div>
+				<div className="modal__status">Success</div>
 			</div>
-			<div className="modal__status">Success</div>
 		</div>
 	);
 }
