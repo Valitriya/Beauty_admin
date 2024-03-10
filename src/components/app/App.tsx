a9de7553eb6fde817472854a56af335f7a9f4c5a
Valentina <Vallitriya@gmail.com>
2024-03-10 18:34:40 +0300

    added file with fake db

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,11 +1,10 @@
 import Header from "../header/Header";
-import CancelModal from "../modal/CancelModal";
+
 import "./app.scss";
 function App() {
 	return (
 		<main className="board">
 			<Header />
-			<CancelModal />
 		</main>
 	);
 }
