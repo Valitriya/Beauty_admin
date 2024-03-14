762c2133fa8fc697253330a2e542fecbf629a252
Valentina <Vallitriya@gmail.com>
2024-03-14 21:26:40 +0300

    deleted express

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -4,12 +4,6 @@ import Header from "../header/Header";
 import useAppointmentService from "../../services/AppointmentService";
 import "./app.scss";
 function App() {
-	const { loadingStatus, getAllAppointments, getAllActiveAppointments } =
-		useAppointmentService();
-		
-	useEffect(() => {
-		getAllAppointments().then((data) => console.log(data));
-	}, []);
 	return (
 		<main className="board">
 			<Header />
