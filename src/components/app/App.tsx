af660d139e965d8475b631228c6563936dd8406a
Valentina <Vallitriya@gmail.com>
2024-03-14 14:12:04 +0300

    added server for working with postman

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import Header from "../header/Header";
 
+import useAppointmentService from "../../services/AppointmentService";
 import "./app.scss";
 function App() {
+	const { loadingStatus, getAllAppointments, getAllActiveAppointments } =
+		useAppointmentService();
+		
+	useEffect(() => {
+		getAllAppointments().then((data) => console.log(data));
+	}, []);
 	return (
 		<main className="board">
 			<Header />
