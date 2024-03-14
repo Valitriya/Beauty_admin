771e3204658bcf9aee5acf918005e70a32c67712
Valentina <Vallitriya@gmail.com>
2024-03-14 23:12:25 +0300

    fixed errors in styles

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,17 +1,32 @@
 import { useEffect } from "react";
 import Header from "../header/Header";
+import SchedulePage from "../../pages/schedule/SchedulePage";
 
 import useAppointmentService from "../../services/AppointmentService";
+import {
+	IAppointment,
+	ActiveAppointment,
+} from "../../shared/interfaces/appointment.interface";
 import "./app.scss";
+
+interface IInitialState {
+	allAppointmens: IAppointment | [];
+	ActiveAppointments: ActiveAppointment | [];
+}
+const initialState: IInitialState = {
+	allAppointmens: [],
+	ActiveAppointments: [],
+};
 function App() {
 	const { loadingStatus, getAllAppointments, getAllActiveAppointments } =
 		useAppointmentService();
 	useEffect(() => {
-		getAllAppointments().then(data => console.log(data))
+		getAllAppointments().then((data) => console.log(data));
 	}, []);
 	return (
 		<main className="board">
 			<Header />
+			<SchedulePage/>
 		</main>
 	);
 }
