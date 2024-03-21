31a61679e32805832c8383b62abff3f699161485
Valentina <Vallitriya@gmail.com>
2024-03-21 20:17:41 +0300

    spinner render and errors written

--- src/components/appointmentList/AppointmentList.tsx
+++ src/components/appointmentList/AppointmentList.tsx
@@ -1,13 +1,33 @@
 import { useEffect, useContext } from "react";
+
 import AppointmentItem from "../appointmentItem/AppointmentItem";
+import Spinner from "../spinner/Spinner";
+import Error from "../error/Error";
 import { AppointmentContext } from "../../context/appointments/AppointmentsContext";
 function AppointmentList() {
-	const { ActiveAppointments, getActiveAppointments } =
-		useContext(AppointmentContext);
+	const {
+		ActiveAppointments,
+		getActiveAppointments,
+		appointmentLoadiingStatus,
+	} = useContext(AppointmentContext);
 
 	useEffect(() => {
 		getActiveAppointments();
 	}, []);
+
+	if (appointmentLoadiingStatus === "loading") {
+		return <Spinner />;
+	} else if (appointmentLoadiingStatus === "error") {
+		return (
+			<>
+				<Error />
+				<button className="schedule__reload" onClick={getActiveAppointments}>
+					Try to reload
+				</button>
+			</>
+		);
+	}
+
 	return (
 		<>
 			{ActiveAppointments.map((item) => {
