c4525af2b7e238e917118ca4de0f2c51fa711237
Valentina <Vallitriya@gmail.com>
2024-03-25 22:43:09 +0300

    complete useLayoutEffect

--- src/components/portal/portal.tsx
+++ src/components/portal/portal.tsx
@@ -2,29 +2,42 @@ import { useState, useLayoutEffect, ReactNode } from "react";
 import { createPortal } from "react-dom";
 
 interface PortalProps {
-    children: ReactNode;
-    wrapperId?: string;
+	children: ReactNode;
+	wrapperId?: string;
 }
 
 function createWrapperAndAppendToBody(wrapperId: string) {
-    const wrapperElement = document.createElement('div');
-    wrapperElement.setAttribute('id', wrapperId);
-    wrapperElement.setAttribute('class', 'modal');
-    document.body.append(wrapperElement);
+	const wrapperElement = document.createElement("div");
+	wrapperElement.setAttribute("id", wrapperId);
+	wrapperElement.setAttribute("class", "modal");
+	document.body.append(wrapperElement);
 
-    return wrapperElement;
+	return wrapperElement;
 }
 
-function Portal({children, wrapperId = 'portal-wrapper'}: PortalProps) {
-    const [wrapperElement, setWrapperElement] = useState< HTMLElement | null>(null);
+function Portal({ children, wrapperId = "portal-wrapper" }: PortalProps) {
+	const [wrapperElement, setWrapperElement] = useState<HTMLElement | null>(
+		null
+	);
 
-    useLayoutEffect(() => {
-        let element = document.getElementById(wrapperId);
+	useLayoutEffect(() => {
+		let element = document.getElementById(wrapperId);
+		let created = false;
 
-        if(!element){
-            element = createWrapperAndAppendToBody(wrapperId);
-        }
-    }, [wrapperId])
+		if (!element) {
+			created = true;
+			element = createWrapperAndAppendToBody(wrapperId);
+		}
+        setWrapperElement(element);
 
-    return createPortal(children, wrapperElement);
+		return () => {
+			if (created) {
+				element?.remove();
+			}
+		};
+	}, [wrapperId]);
+
+	if (wrapperElement === null) return null;
+
+	return createPortal(children, wrapperElement);
 }
