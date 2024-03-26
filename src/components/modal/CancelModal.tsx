8467fc5e85889952c91544778b9c9b74571a44c5
Valentina <Vallitriya@gmail.com>
2024-03-26 22:13:53 +0300

    added functionality to delete an element

--- src/components/modal/CancelModal.tsx
+++ src/components/modal/CancelModal.tsx
@@ -1,13 +1,34 @@
-import { useEffect } from "react";
+import { useEffect, useState, useContext } from "react";
 import Portal from "../portal/portal";
+import useAppointmentService from "../../services/AppointmentService";
 import "./modal.scss";
+import { AppointmentContext } from "../../context/appointments/AppointmentsContext";
 
 interface IModalProps {
 	handleClose: (state: boolean) => void;
 	selectedId: number;
 }
 
-function CancelModal({ handleClose }: IModalProps) {
+function CancelModal({ handleClose, selectedId }: IModalProps) {
+	const { getActiveAppointments } = useContext(AppointmentContext);
+
+	const { cancelOneAppointment } = useAppointmentService();
+
+	const [btnDisabled, setBtnDisabled] = useState<boolean>(false);
+	const [cancelStatus, setCancelStatus] = useState<boolean | null>(null);
+
+	const handleCancelAppointment = (id: number) => {
+		setBtnDisabled(true);
+		cancelOneAppointment(id)
+			.then(() => {
+				setCancelStatus(true);
+			})
+			.catch(() => {
+				setCancelStatus(false);
+				setBtnDisabled(false);
+			});
+	};
+
 	const closeOnEscape = (e: KeyboardEvent): void => {
 		if (e.key === "Escape") {
 			handleClose(false);
@@ -29,14 +50,24 @@ function CancelModal({ handleClose }: IModalProps) {
 						Are you sure you want to delete the appointment?
 					</span>
 					<div className="modal__btns">
-						<button className="modal__ok" onClick={() => handleClose(false)}>
+						<button
+							className="modal__ok"
+							disabled={btnDisabled}
+							onClick={() => handleCancelAppointment(selectedId)}
+						>
 							Ok
 						</button>
 						<button className="modal__close" onClick={() => handleClose(false)}>
 							Close
 						</button>
 					</div>
-					<div className="modal__status">Success</div>
+					<div className="modal__status">
+						{cancelStatus === null
+							? ""
+							: cancelStatus
+							? "Success"
+							: "Error, please try again"}
+					</div>
 				</div>
 			</div>
 		</Portal>
